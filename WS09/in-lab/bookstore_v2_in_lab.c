36254e2a00ddd174a0358b4aebddd7614b29ea53
LupodeLupis <35709073+LupodeLupis@users.noreply.github.com>
2018-01-24 17:56:05 -0500

    Update bookstore_v2_in_lab.c

--- WS09/in-lab/bookstore_v2_in_lab.c
+++ WS09/in-lab/bookstore_v2_in_lab.c
@@ -1,10 +1,3 @@
-// ---------------------------------------
-// Name: ALBERTO LAURENZI
-// ID:150954162
-// Email:alaurenzi@myseneca.ca
-// Workshop:Lab_09
-// ---------------------------------------
-
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #define MAX_TITLE_SIZE 20
